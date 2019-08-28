06f30c744fa2c929b9811f378dd74b3b2612e9d8
jim.jia <zhenshanjia@foxmail.com>
2019-08-28 21:19:47 +0800

    create

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {GlobalStyle} from './style'
+import Header from './common/header/'
+
+function App() {
+  return (
+    <div className="App dell">
+      <GlobalStyle />
+      <Header />
+      hello world
+    </div>
+  );
+}
+
+export default App;
